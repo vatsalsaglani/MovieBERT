ae9c01449f6a65c5a1dee1fb3302512adcf151ed
vatsalsaglani <saglanivatsal@gmail.com>
2022-08-21 13:36:34 +0530

    added loading animation to movies page

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,19 @@
-export const Header = () => (
+export const Header = ({ loading }) => (
   <div className="navbar bg-slate-800 rounded-lg shadow-lg">
-    <a className="btn btn-ghost normal-case font-comforta text-teal-50 text-xl">
-      MovieBERT
-    </a>
+    {loading ? (
+      <div
+        className="text-2xl font-comforta font-semibold 
+      bg-gradient-to-r bg-clip-text  text-transparent 
+      from-slate-200 via-teal-800 to-slate-200
+      animate-text
+      "
+      >
+        MovieBert
+      </div>
+    ) : (
+      <a className="btn btn-ghost normal-case font-comforta text-teal-50 text-xl">
+        MovieBERT
+      </a>
+    )}
   </div>
 );
