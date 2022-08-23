a38f36b69d8610c81497fd91e04459969a1423fd
vatsalsaglani <saglanivatsal@gmail.com>
2022-08-23 18:30:34 +0530

    fixed named and added letter spacing

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,14 @@ export const Header = ({ loading }) => (
       <div
         className="text-2xl font-comforta font-semibold 
       bg-gradient-to-r bg-clip-text  text-transparent 
-      from-slate-200 via-teal-800 to-slate-200
+      from-slate-200 via-teal-800 to-slate-200 tracking-widest
       animate-text
       "
       >
-        MovieBert
+        MovieBERT
       </div>
     ) : (
-      <a className="btn btn-ghost normal-case font-comforta text-teal-50 text-xl">
+      <a className="btn btn-ghost normal-case font-comforta transition-opacity text-teal-50 text-xl">
         MovieBERT
       </a>
     )}
